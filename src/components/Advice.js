86013977d136a7d839d3b6e8c7f120df9a1335cd
Rajeshkumarachari <131753283+Rajeshkumarachari@users.noreply.github.com>
2024-02-02 10:00:09 +0530

    advce complted

--- src/components/Advice.js
+++ src/components/Advice.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Advice = () => {
-  return <div>Advice</div>;
+  const [advice, setAdvice] = useState("Please click to get Advice");
+  const [count, setCount] = useState(0);
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
+  }
+  useEffect(() => {
+    getAdvice();
+  }, []);
+
+  return (
+    <div className=" flex justify-center items-center bg-red-300  h-screen w-screen ">
+      <div className=" text-center">
+        <h1 className=" text-3xl text-purple-700 font-semibold">{advice}</h1>
+        <br />
+        <button
+          onClick={getAdvice}
+          className=" px-3 py-2 bg-blue-800 text-white rounded-sm hover:bg-blue-900  hover:shadow-sm"
+        >
+          Get Advice
+        </button>
+        <p className=" my-3 text-xl text-gray-700">
+          You get {count} {count == 1 ? "Advice" : "Advices"}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Advice;
