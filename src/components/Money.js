b1709e1a2aa007fb1695cad9259382ec1b5fc528
Rajeshkumarachari <131753283+Rajeshkumarachari@users.noreply.github.com>
2024-02-04 16:34:05 +0530

    all done

--- src/components/Money.js
+++ src/components/Money.js
@@ -1,10 +1,41 @@
-import React, { useState } from "react";
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 const Money = () => {
   const [amount, setAmount] = useState(1);
   const [fromCurrency, setFromCurrency] = useState("USD");
   const [toCurrency, setToCurrency] = useState("INR");
   const [convertedAmount, setConvertedAmount] = useState(null);
+  const [exchangeRate, setEchangeRate] = useState(null);
+  useEffect(() => {
+    const getEchangeRate = async () => {
+      try {
+        let url = `https://api.exchangerate-api.com/v4/latest/${fromCurrency}`;
+        const response = await axios.get(url);
+        // console.log(response);
+        setEchangeRate(response.data.rates[toCurrency]);
+      } catch (error) {
+        console.log("Error fetching exchange rate:", error);
+      }
+    };
+    getEchangeRate();
+  }, [fromCurrency, toCurrency]);
+
+  useEffect(() => {
+    if (exchangeRate !== null) {
+      setConvertedAmount((amount * exchangeRate).toFixed(2));
+    }
+  }, [amount, exchangeRate]);
+
+  const handleAmountChange = (e) => {
+    const value = parseFloat(e.target.value);
+    setAmount(isNaN(value) ? 0 : value);
+  };
+  const handelFromCurrency = (e) => {
+    setFromCurrency(e.target.value);
+  };
+  const handleToCurrencyChange = (e) => {
+    setToCurrency(e.target.value);
+  };
   return (
     <div className="w-[100vw] h-[100vh] flex justify-center  items-center bg-gray-500 ">
       <div className=" bg-gray-300 rounded-md w-[500px] px-4 py-4 ">
@@ -19,6 +50,8 @@ const Money = () => {
             <input
               type="text"
               id="amt"
+              value={amount}
+              onChange={handleAmountChange}
               className=" border rounded-md border-gray-500 my-3 ml-[66px] w-[220px] py-1 mx-5 bg-green-50  outline-none pl-2"
             />
           </div>
@@ -26,6 +59,8 @@ const Money = () => {
             <label htmlFor="fromCurrency">From Currency: </label>
             <select
               id="fromCurrency "
+              value={fromCurrency}
+              onChange={handelFromCurrency}
               className=" border border-gray-500 rounded-md my-3 py-1 mx-5 pl-2 bg-green-50"
             >
               <option value="USD">USD - United States Dollar</option>
@@ -43,6 +78,8 @@ const Money = () => {
           <div>
             <label htmlFor="toCurrency">To Currency: </label>
             <select
+              value={toCurrency}
+              onChange={handleToCurrencyChange}
               id="toCurrency"
               className=" border border-gray-500 rounded-md my-3 py-1 mx-5 ml-10 pl-2 bg-green-50"
             >
@@ -60,8 +97,7 @@ const Money = () => {
           </div>
           <div>
             <p className=" border border-blue-400 py-2 px-3 rounded-md text-lg  font-semibold">
-              {" "}
-              Amount
+              {amount} {fromCurrency} is equal to {convertedAmount} {toCurrency}
             </p>
           </div>
         </div>
